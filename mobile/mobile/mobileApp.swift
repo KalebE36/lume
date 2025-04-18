71a4e7904ca14719a177744539d2f4be85fc9cdc
KalebE36 <kaleb.ellington33@gmail.com>
2025-04-18 12:05:54 -0400

    init commit

---
+++ mobile/mobile/mobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  mobileApp.swift
+//  mobile
+//
+//  Created by Kaleb Ellington on 4/18/25.
+//
+
+import SwiftUI
+
+@main
+struct mobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ mobile/mobileTests/mobileTests.swift
@@ -0,0 +1,17 @@
+//
+//  mobileTests.swift
+//  mobileTests
+//
+//  Created by Kaleb Ellington on 4/18/25.
+//
+
+import Testing
+@testable import mobile
+
+struct mobileTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
