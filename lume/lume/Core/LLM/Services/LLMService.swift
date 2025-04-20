ccf26fa6a996258fcf6522149261cffc55d5cd9d
KalebE36 <kaleb.ellington33@gmail.com>
2025-04-19 21:47:14 -0400

    struct changes

--- lume/lume/Core/LLM/Services/LLMService.swift
+++ lume/lume/Core/LLM/Services/LLMService.swift
@@ -3,7 +3,6 @@
 
     class LLMService {
         private let apiKey: String
-        private let baseStringURL: String = ""
         
         
         
@@ -14,7 +13,9 @@
         
         func receiveGemResponse(body: GemModelRequest) async throws -> GemModelResponse {
             
-            guard let url = URL(string: baseStringURL) else {
+            
+            /* ModelEndpoints defined in the APIConfig within config dir in / */
+            guard let url = ModelEndpoints.gem.url() else {
                 throw GemError.invalidURL
             }
             
@@ -22,7 +23,7 @@
             request.httpMethod = "POST"
             request.setValue("application/json", forHTTPHeaderField: "Content-Type")
             
-            // Need to encode the body from the data class
+            /* Need to encode the body from the data class */
             request.httpBody = try JSONEncoder().encode(body)
             
             let (data, response) = try await URLSession.shared.data(for: request)
@@ -33,7 +34,7 @@
             
             
             
-            
+        
             do {
                 let decoder = JSONDecoder()
                 return try decoder.decode(GemModelResponse.self, from: data)
@@ -45,7 +46,7 @@
         
         func recieveDeepResponse(body: DeepModelRequest) async throws -> DeepModelResponse {
             
-            guard let url = URL(string: baseStringURL) else {
+            guard let url = ModelEndpoints.deep.url() else {
                 throw DeepError.invalidURL
             }
             
@@ -59,6 +60,13 @@
             
             guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
                 throw DeepError.invalidResponse
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                return try decoder.decode(DeepModelResponse.self, from: data)
+            } catch {
+                throw DeepError.invalidData
             }
             
         }
